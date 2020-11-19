6086c1e764ba2194f85cd16e1cc86ea6f493bac3
pathakakshay <pathakakshay1111@gmail.com>
2020-11-19 10:55:29 +0530

    presenatation and conatiner layer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
 import {DISHES } from './shared/dishes';
 import { Component } from 'react';
@@ -16,12 +15,7 @@ class App extends Component{
   render() {
     return (
         <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes = { this.state.dishes }/>
+          <Main />
         </div>
     );
   }
