cfd19e247967781e9660af0bf652b7a493ba2036
pathakakshay <pathakakshay1111@gmail.com>
2020-11-21 10:51:14 +0530

    spa-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishDetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent';
@@ -31,6 +32,12 @@ class Main extends Component {
           />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header />
@@ -38,6 +45,7 @@ class Main extends Component {
             <Route path="/home" component= { HomePage } />
             <Route exact path= "/menu" component= {() => <Menu dishes ={this.state.dishes}/> } />
             <Route exact path="/contactus" component={ Contact } />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to ="/home" />
           </Switch>
         <Footer />
